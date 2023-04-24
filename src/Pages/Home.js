50c226dfc61e2b1f76df5cb3dc2664f6221ea4a5
sathish <2000sathishkumar.d@gmail.com>
2023-04-24 21:42:23 +0530

    Update data.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,15 @@
 import React from "react";
+// import Data file
+import { CategoryBarData, CarouselData } from "../Data";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <div className="home-categoryContainer">
+        <div className="home-categoryBar"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/components/CategoryBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const CategoryBar = () => {
+  return (
+    <div className="categoryBar">
+      <div className="category-img">
+        <img src={Imgsrc} alt="CategoryImage" />
+      </div>
+    </div>
+  );
+};
+
+export default CategoryBar;
