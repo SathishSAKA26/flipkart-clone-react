e1ac9aaf0752792b4ef432bdfe30072e13f3c6d0
sathish <2000sathishkumar.d@gmail.com>
2023-04-26 10:41:08 +0530

    Update ProductCarousel.js

--- src/components/ProductCarousel.js
+++ src/components/ProductCarousel.js
@@ -5,8 +5,31 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { Link } from "react-router-dom";
 import CategoryBanner from "./CategoryBanner";
+// import react icons
+import { MdArrowCircleLeft, MdArrowCircleRight } from "react-icons/md";
 
-const
+// Next and Prev Button function
+const Next = (props) => {
+  const { className, onClick } = props;
+  return (
+    <div className={className} onClick={onClick}>
+      <MdArrowCircleRight
+        style={{ color: "black", fondSize: 25, fondWeight: 900 }}
+      />
+    </div>
+  );
+};
+
+const Prev = (props) => {
+  const { className, onClick } = props;
+  return (
+    <div className={className} onClick={onClick}>
+      <MdArrowCircleLeft
+        style={{ color: "black", fondSize: 25, fondWeight: 900 }}
+      />
+    </div>
+  );
+};
 
 const ProductCarousel = ({ BgIm, Title, Data }) => {
   const settings = {
@@ -26,7 +49,7 @@ const ProductCarousel = ({ BgIm, Title, Data }) => {
         <button className="categoryCarousel-btn">View All</button>
       </div>
       <div className="categoryCarousel-right">
-        <Slider {...settings}>
+        <Slider nextArrow={<Next />} prevArrow={<Prev />} {...settings}>
           {Data.map((item, index) => (
             <Link key={index} to={"/Products"}>
               <CategoryBanner
